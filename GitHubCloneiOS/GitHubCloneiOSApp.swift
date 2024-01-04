d5291df57edd11169d14ed6e5fa20d71fbd88845
Clement Moleko <molekoclement@gmail.com>
2024-01-04 05:20:26 +0200

    Initial Commit

---
+++ GitHubCloneiOS/GitHubCloneiOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubCloneiOSApp.swift
+//  GitHubCloneiOS
+//
+//  Created by Clement Skosana on 2024/01/04.
+//
+
+import SwiftUI
+
+@main
+struct GitHubCloneiOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
