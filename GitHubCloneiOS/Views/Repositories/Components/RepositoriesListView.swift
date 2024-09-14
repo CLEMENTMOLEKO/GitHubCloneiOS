f7eea3ac454a1ab3ee97bc3cff6e4b610e2746d7
Clement Moleko <molekoclement@gmail.com>
2024-09-14 20:37:14 +0200

    Fix tap gesture issue when routing to repository detail

--- GitHubCloneiOS/Views/Repositories/Components/RepositoriesListView.swift
+++ GitHubCloneiOS/Views/Repositories/Components/RepositoriesListView.swift
@@ -16,44 +16,45 @@ struct RepositoriesListView: View {
         List {
             ForEach(repositories) { repository in
                 VStack(alignment: .leading, spacing: 10) {
-                    Group {
-                        HStack {
-                            CachedAvatarView(
-                                imageKey: "\(repository.owner.id)",
-                                imageUrl: repository.owner.avatarURL ?? ""
-                            )
-                            .frame(width: 30)
-                            Text(repository.owner.login)
-                                .font(.callout)
-                        }
-                        Text(repository.name)
-                            .font(.headline)
-                        if let description = repository.description {
-                            Text(description)
-                                .font(.subheadline)
-                        }
-                        HStack {
-                            if let stargazerCount = repository.stargazersCount,
-                                stargazerCount > 0 {
-                                Image(systemName: "star")
-                                    .symbolVariant(.fill)
-                                    .foregroundStyle(.yellow)
-                            } else {
-                                Image(systemName: "star")
-                                    .foregroundColor(.secondary)
-                            }
-                            
-                            Text("\(repository.stargazersCount ?? 0)")
-                        }
-                    }
-                    .onTapGesture {
-                        navigationManager.navigate(
-                            to: .repositoryDetail(
-                                ownerName: repository.owner.login,
-                                repoName: repository.name
-                            )
+                    HStack {
+                        CachedAvatarView(
+                            imageKey: "\(repository.owner.id)",
+                            imageUrl: repository.owner.avatarURL ?? ""
                         )
+                        .frame(width: 30)
+                        Text(repository.owner.login)
+                            .font(.callout)
+                        Spacer()
                     }
+                    Text(repository.name)
+                        .font(.headline)
+                    if let description = repository.description {
+                        Text(description)
+                            .font(.subheadline)
+                    }
+                    HStack {
+                        if let stargazerCount = repository.stargazersCount,
+                            stargazerCount > 0 {
+                            Image(systemName: "star")
+                                .symbolVariant(.fill)
+                                .foregroundStyle(.yellow)
+                        } else {
+                            Image(systemName: "star")
+                                .foregroundColor(.secondary)
+                        }
+                        
+                        Text("\(repository.stargazersCount ?? 0)")
+                    }
+                }
+                .frame(maxWidth: .infinity)
+                .contentShape(Rectangle())
+                .onTapGesture {
+                    navigationManager.navigate(
+                        to: .repositoryDetail(
+                            ownerName: repository.owner.login,
+                            repoName: repository.name
+                        )
+                    )
                 }
             }
             
